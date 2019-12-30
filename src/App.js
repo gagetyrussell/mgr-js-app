6eaf8d16ef4691cb5162b8a6a9293a24d04d5e1d
gagetyrussell <gagetyrussell@gmail.com>
2019-12-29 18:36:31 -0600

    added login routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App(props) {
   return (
@@ -10,14 +11,25 @@ function App(props) {
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/">Home</Link>
+            <Link to="/">MGR</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Config.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://qcp3xrmpmj.execute-api.us-east-1.amazonaws.com/dev/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_AhleHc0IP",
+    APP_CLIENT_ID: "7snnj35ibuk6nhd44kgi377m63",
+    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+  }
+};
