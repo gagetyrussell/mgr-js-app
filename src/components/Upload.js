15b1d6aa284305900559ddd7cdf3172556876a5e
gagetyrussell <gagetyrussell@gmail.com>
2019-12-31 14:36:04 -0600

    s3 upload working

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,5 +1,9 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useCallback, useEffect, useState} from 'react';
 import {useDropzone} from 'react-dropzone';
+import MGRAPI from "../utils/MGRAPI";
+import { Auth } from "aws-amplify";
+import axios from "axios";
+
 
 const baseStyle = {
   flex: 1,
@@ -29,33 +33,117 @@ const rejectStyle = {
   borderColor: '#ff1744'
 };
 
-export default function Accept(props) {
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject
-  } = useDropzone({
-    accept: 'application/vnd.ms-excel, text/plain'
-  });
+function getSignedUrl(user_id, filename) {
+  let signed_url = MGRAPI.get('/getPresignedUserDataUrl', {
+    params: {
+      user_id: user_id,
+      file_name: filename
+    }
+  })
+  // const params = {
+  //   objectName: file.name,
+  //   contentType: file.type
+  // };
 
-  const style = useMemo(() => ({
-    ...baseStyle,
-    ...(isDragActive ? activeStyle : {}),
-    ...(isDragAccept ? acceptStyle : {}),
-    ...(isDragReject ? rejectStyle : {})
-  }), [
-    isDragActive,
-    isDragReject
-  ]);
+  // .then(data => {
+  //   callback(data);
+  // })
+  // .catch(error => {
+  //   console.error(error);
+  // });
+}
+
+function getUserName() {
+  Auth.currentAuthenticatedUser()
+  .then(user => {
+    return user;
+  })
+};
+
+
+function MyDropzone(user) {
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+      // Do whatever you want with the file contents
+        const binaryStr = reader.result
+        Auth.currentAuthenticatedUser().then(async result => {
+          let signed_url = await MGRAPI.get('/getPresignedUserDataUrl', {
+              params: {
+                user_id: result.username,
+                file_name: file.name
+              }
+            })
+          console.log(signed_url.data.fields)
+          const formData = new FormData();
+          Object.keys(signed_url.data.fields).forEach(key => {
+            formData.append(key, signed_url.data.fields[key]);
+          });
+          formData.append("file", file);
+          var options = { headers: { 'Content-Type': file.type, 'x-amz-acl': 'public-read' } };
+          let http_post = axios({
+            method: 'post',
+            url: signed_url.data.url,
+            data: formData,
+            options: options
+          });
+          console.log(file)
+          console.log(result.username)
+          return result;
+
+
+            // do stuff with axios now
+
+            //userdata = useState(result)
+            // console.log('gage',result)
+        });
+
+        //console.log(file.name)
+        //console.log(binaryStr)
+      }
+
+      reader.readAsArrayBuffer(file)
+    })
+
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop, accept: 'application/vnd.ms-excel, text/plain'})
 
   return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <p>Drag 'n' drop some files here, or click to select files</p>
+    </div>
+  )
+};
+
+
+
+// MovieLibrary.getGenres = function() {
+//   var promise = new Promise(function(resolve, reject) {
+//     /* missing implementation */
+//     resolve(result);
+//   });
+//
+//   return promise;
+// };
+//
+
+
+export default function Accept(props) {
+  // getUserName()
+  const useDropZone = MyDropzone()
+  // console.log(useDropZone)
+  // Auth.currentAuthenticatedUser()
+  // .then(function(user) {
+  //   return user;
+  // })
+  // .catch(() => console.log("Not signed in"));
+  return (
     <div className="container">
-      <div {...getRootProps({style})}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      </div>
+      { useDropZone }
     </div>
   );
 }
