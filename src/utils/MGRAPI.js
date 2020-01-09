6989c87ef0b3a45e32fc3499ee4046818a00c406
gagetyrussell <gagetyrussell@gmail.com>
2020-01-08 22:50:56 -0600

    fix

--- src/utils/MGRAPI.js
+++ src/utils/MGRAPI.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import config from '../config';
 
 export default axios.create({
-  baseURL: config.LOCAL_MGR_API_URL,
+  baseURL: config.MGR_API_URL,
   responseType: "json"
 });
