79d99cbeb6581ee1d3422347fbbd3ffdcc53395c
Ronnie Dove <radove@gmail.com>
2020-01-12 00:19:54 -0500

    Updates

--- src/containers/ChartEditor2.js
+++ src/containers/ChartEditor2.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import plotly from 'plotly.js/dist/plotly';
 import PlotlyEditor from 'react-chart-editor';
 import 'react-chart-editor/lib/react-chart-editor.css';
@@ -9,107 +9,127 @@ import MGRAPI from "../utils/MGRAPI";
 import { Auth } from "aws-amplify";
 
 const dataSourceOptions = Object.keys(dataSources).map(name => ({
-  value: name,
-  label: name,
+    value: name,
+    label: name,
 }));
 
-const config = {editable: true};
+const config = { editable: true };
 
 class ChartEditor2 extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      data: [],
-      layout: {},
-      frames: [],
-      currentMockIndex: -1,
-      mocks: [],
-    };
+        this.state = {
+            data: [],
+            layout: {},
+            frames: [],
+            currentMockIndex: -1,
+            mocks: [],
+        };
 
-    this.loadMock = this.loadMock.bind(this);
-  }
+        this.loadMock = this.loadMock.bind(this);
+        this.refreshData = this.refreshData.bind(this);
+    }
 
-  componentWillMount() {
-    Auth.currentAuthenticatedUser().then(async result => {
-      let mock_objects = await MGRAPI.get('/getSavedChartsByUser', {
-          params: {
-            user_id: result.username,
-          }
-        })
-        const mocks = mock_objects.data
-        console.log('mocks:',mocks)
-        console.log('mocks.data:',mocks.data)
-        this.setState({mocks})
-        console.log('state:',this.state)
-      });
-//     fetch('https://api.github.com/repos/plotly/plotly.js/contents/test/image/mocks')
-//       .then(response =>  response.json())
-//       .then(mocks => this.setState({mocks}));
-  }
-
-  loadMock(mockIndex) {
-    console.log(mockIndex);
-    const mock = this.state.mocks[mockIndex];
-    console.log(mock.key)
-    Auth.currentAuthenticatedUser().then(async result => {
-      let figure = await MGRAPI.get('/getSavedChartJson', {
-          params: {
-            key: mock.key,
-          }
-        })
-        console.log('file', figure);
-        this.setState({
-          currentMockIndex: mockIndex,
-          data: Object.keys(figure.data.data).map(i => figure.data.data[i]),
-          layout: figure.data.layout,
-          frames: figure.frames,
+    refreshData() {
+        Auth.currentAuthenticatedUser().then(async result => {
+            let mock_objects = await MGRAPI.get('/getSavedChartsByUser', {
+                params: {
+                    user_id: result.username,
+                }
+            })
+            const mocks = mock_objects.data
+            console.log('mocks:', mocks)
+            console.log('mocks.data:', mocks.data)
+            this.setState({ mocks })
+            console.log('state:', this.state)
         });
-      });
-//     fetch(mock.url, {
-//       headers: new Headers({Accept: 'application/vnd.github.v3.raw'}),
-//     })
-//       .then(response => response.json())
-//       .then(figure => {
+        //     fetch('https://api.github.com/repos/plotly/plotly.js/contents/test/image/mocks')
+        //       .then(response =>  response.json())
+        //       .then(mocks => this.setState({mocks}));
+    }
+
+    componentWillMount() {
+        Auth.currentAuthenticatedUser().then(async result => {
+            let mock_objects = await MGRAPI.get('/getSavedChartsByUser', {
+                params: {
+                    user_id: result.username,
+                }
+            })
+            const mocks = mock_objects.data
+            console.log('mocks:', mocks)
+            console.log('mocks.data:', mocks.data)
+            this.setState({ mocks })
+            console.log('state:', this.state)
+        });
+        //     fetch('https://api.github.com/repos/plotly/plotly.js/contents/test/image/mocks')
+        //       .then(response =>  response.json())
+        //       .then(mocks => this.setState({mocks}));
+    }
+
+    loadMock(mockIndex) {
+        console.log(mockIndex);
+        const mock = this.state.mocks[mockIndex];
+        console.log(mock.key)
+        Auth.currentAuthenticatedUser().then(async result => {
+            let figure = await MGRAPI.get('/getSavedChartJson', {
+                params: {
+                    key: mock.key,
+                }
+            })
+            console.log('file', figure);
+            this.setState({
+                currentMockIndex: mockIndex,
+                data: Object.keys(figure.data.data).map(i => figure.data.data[i]),
+                layout: figure.data.layout,
+                frames: figure.frames,
+            });
+        });
+        //     fetch(mock.url, {
+        //       headers: new Headers({Accept: 'application/vnd.github.v3.raw'}),
+        //     })
+        //       .then(response => response.json())
+        //       .then(figure => {
         // this.setState({
         //   currentMockIndex: mockIndex,
         //   data: figure.data,
         //   layout: figure.layout,
         //   frames: figure.frames,
         // });
-//         console.log(this.state)
-//       });
-  }
+        //         console.log(this.state)
+        //       });
+    }
 
-  render() {
-    console.log(this.state)
-    return (
-      <div className="app">
-        <PlotlyEditor
-          data={this.state.data}
-          layout={this.state.layout}
-          config={config}
-          frames={this.state.frames}
-          dataSources={dataSources}
-          dataSourceOptions={dataSourceOptions}
-          plotly={plotly}
-          onUpdate={(data, layout, frames) => this.setState({data, layout, frames})}
-          useResizeHandler
-          debug
-          advancedTraceTypeSelector
-        />
-        <UploadChart
-          data={this.state.data}
-          layout={this.state.layout}
-        />
-        <Nav
-          currentMockIndex={this.state.currentMockIndex}
-          loadMock={this.loadMock}
-          mocks={this.state.mocks}
-        />
-      </div>
-    );
-  }
+    render() {
+        console.log(this.state)
+        return (
+            <div className="app">
+                <PlotlyEditor
+                    data={this.state.data}
+                    layout={this.state.layout}
+                    config={config}
+                    frames={this.state.frames}
+                    dataSources={dataSources}
+                    dataSourceOptions={dataSourceOptions}
+                    plotly={plotly}
+                    onUpdate={(data, layout, frames) => this.setState({ data, layout, frames })}
+                    useResizeHandler
+                    debug
+                    advancedTraceTypeSelector
+                />
+                <UploadChart
+                    refreshData={this.refreshData}
+                    data={this.state.data}
+                    layout={this.state.layout}
+                />
+                <Nav
+                    currentMockIndex={this.state.currentMockIndex}
+                    loadMock={this.loadMock}
+                    mocks={this.state.mocks}
+                />
+            </div>
+        );
+    }
 }
 
 export default ChartEditor2;
