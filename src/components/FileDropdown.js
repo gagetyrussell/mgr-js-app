fa744e4942a6e531d2dc9ab306b5a947c5d4342d
gagetyrussell <gagetyrussell@gmail.com>
2020-01-12 18:08:42 -0600

    user data working on chart editor

--- src/components/FileDropdown.js
+++ src/components/FileDropdown.js
@@ -3,36 +3,34 @@ import MGRAPI from "../utils/MGRAPI";
 import { Auth } from "aws-amplify";
 import axios from "axios";
 import _ from 'lodash';
-import { Dropdown, Menu, Icon, message } from 'antd';
+import { Dropdown, Menu, Icon, message, Select } from 'antd';
 
-function dataToCharts(data){
-  return ( data.map((item, index) => (
-    <Menu.Item key={index}>
-        <Icon type="user" />
-        {item}
-      </Menu.Item>
+const { Option } = Select;
+
+function dataToDropdown(data) {
+  console.log("data",data)
+  return ( data.map((item) => (
+    <Option key={item.key}>
+      {item.name}
+    </Option>
     ))
   )
 };
 
-class FileTable extends React.Component {
+export default class FileDropdown extends React.Component {
   constructor(props) {
     super(props);
-    this.props = props;
     this.state = {
       files: {},
-      hasData: false,
-      data: {}
-    };
-    console.log(this.props)
-
-    //this.dataToCharts = this.dataToCharts.bind(this);
-    this.filterQueryRefresh = _.debounce(this.filterQueryRefresh.bind(this), 200);
+      hasData: false
+    }
     this.refreshData = this.refreshData.bind(this);
+    //this.dataToDropdown = this.dataToDropdown.bind(this);
   }
+
   refreshData() {
     Auth.currentAuthenticatedUser().then(async result => {
-      let files = await MGRAPI.get('/listDataByUser', {
+      let files = await MGRAPI.get('/getDataByUser', {
           params: {
             user_id: result.username,
           }
@@ -41,59 +39,166 @@ class FileTable extends React.Component {
         this.setState({ hasData: true })
       })
   }
-  filterQueryRefresh() {
-    Auth.currentAuthenticatedUser().then(async result => {
-      let files = await MGRAPI.get('/listDataByUser', {
-          params: {
-            user_id: result.username,
-          }
-        })
-        var data = files.database
-        this.setState({ hasData: true })
-        this.setState({ files })
-      })
-  }
+
   componentDidMount() {
     this.refreshData();
   }
 
   render() {
-    function handleMenuClick(e) {
-      message.info('Click on menu item.');
-      console.log('click', e);
-    }
-    var menu = (
-      <Menu onClick={handleMenuClick}>
-        <Menu.Item key="1">
-          <Icon type="user" />
-          scatter
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="user" />
-          bar
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="user" />
-          box
-        </Menu.Item>
-      </Menu>
-    )
+    // function handleChange(e) {
+    //   message.info('Click on menu item.');
+    //   console.log('click', e[0]);
+    //   Auth.currentAuthenticatedUser().then(async result => {
+    //     let file_data = await MGRAPI.get('/parseDataFromFile', {
+    //         params: {
+    //           file_key: e[0],
+    //         }
+    //       })
+    //       console.log(file_data)
+    //     })
+    // }
+
+    var menu;
     if (this.state.hasData) {
-      let menu_items = dataToCharts(this.state.files.data)
-      console.log(menu_items)
-      var menu = (
-        <Menu onClick={handleMenuClick}>
-          //{dataToCharts(this.state.files.data)}
-        </Menu>
+      console.log('bool', this.state.hasData)
+      menu = (
+        <Select
+          mode="multiple"
+          style={{ width: '100%' }}
+          placeholder="Please select data sources"
+          onChange={this.props.handleChange}
+          >
+            {dataToDropdown(this.state.files.data)}
+        </Select>
+      );
+
+    } else {
+      menu = (
+        <Select
+          mode="multiple"
+          style={{ width: '100%' }}
+          placeholder="Please select"
+          onChange={this.props.handleChange}
+          >
+        </Select>
       );
     }
+
     return (
-        <Dropdown.Button overlay={menu} >
-          Select Data Source
-        </Dropdown.Button>
-        //data={this.state.files.data}
+      <div>
+        {menu}
+      </div>
     )
   }
 }
 
-export default FileTable;
+//     if (this.state.hasData) {
+//       let menu_items = dataToCharts(this.state.files.data)
+//       console.log(menu_items)
+//       var menu = (
+//         <Menu onClick={handleMenuClick}>
+//           //{dataToCharts(this.state.files.data)}
+//         </Menu>
+//       );
+//     }
+//     return (
+//         <Dropdown.Button overlay={menu} >
+//           Select Data Source
+//         </Dropdown.Button>
+//         //data={this.state.files.data}
+//     )
+//   }
+
+
+// function dataToCharts(data){
+//   return ( data.map((item, index) => (
+//     <Menu.Item key={index}>
+//         <Icon type="user" />
+//         {item}
+//       </Menu.Item>
+//     ))
+//   )
+// };
+//
+// class FileTable extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.props = props;
+//     this.state = {
+//       files: {},
+//       hasData: false,
+//       data: {}
+//     };
+//     console.log(this.props)
+//
+//     //this.dataToCharts = this.dataToCharts.bind(this);
+//     this.filterQueryRefresh = _.debounce(this.filterQueryRefresh.bind(this), 200);
+//     this.refreshData = this.refreshData.bind(this);
+//   }
+//   refreshData() {
+//     Auth.currentAuthenticatedUser().then(async result => {
+//       let files = await MGRAPI.get('/getDataByUser', {
+//           params: {
+//             user_id: result.username,
+//           }
+//         })
+//         this.setState({ files })
+//         this.setState({ hasData: true })
+//       })
+//   }
+//   filterQueryRefresh() {
+//     Auth.currentAuthenticatedUser().then(async result => {
+//       let files = await MGRAPI.get('/listDataByUser', {
+//           params: {
+//             user_id: result.username,
+//           }
+//         })
+//         var data = files.database
+//         this.setState({ hasData: true })
+//         this.setState({ files })
+//       })
+//   }
+//   componentDidMount() {
+//     this.refreshData();
+//   }
+//
+//   render() {
+//     function handleMenuClick(e) {
+//       message.info('Click on menu item.');
+//       console.log('click', e);
+//     }
+//     var menu = (
+//       <Menu onClick={handleMenuClick}>
+//         <Menu.Item key="1">
+//           <Icon type="user" />
+//           scatter
+//         </Menu.Item>
+//         <Menu.Item key="2">
+//           <Icon type="user" />
+//           bar
+//         </Menu.Item>
+//         <Menu.Item key="3">
+//           <Icon type="user" />
+//           box
+//         </Menu.Item>
+//       </Menu>
+//     )
+//     if (this.state.hasData) {
+//       let menu_items = dataToCharts(this.state.files.data)
+//       console.log(menu_items)
+//       var menu = (
+//         <Menu onClick={handleMenuClick}>
+//           //{dataToCharts(this.state.files.data)}
+//         </Menu>
+//       );
+//     }
+//     return (
+//         <Dropdown.Button overlay={menu} >
+//           Select Data Source
+//         </Dropdown.Button>
+//         //data={this.state.files.data}
+//     )
+//   }
+// }
+//
+// export default FileTable;
